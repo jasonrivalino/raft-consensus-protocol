458c9903c96a95b79eb837acb611c838ec184f60
jasonrivalino <13521008@std.stei.itb.ac.id>
2024-05-30 17:18:37 +0700

    feat: initiate code from references

---
+++ src/lib/app.py
@@ -0,0 +1,37 @@
+class Application:
+    def __init__(self):
+        self.data = {}
+
+    def ping(self):
+        print("PONG")
+
+    def get(self, key):
+        return self.data.get(key, "")
+
+    def set(self, key, value):
+        self.data[key] = value
+        return "OK"
+
+    def strln(self, key):
+        value = self.data.get(key, "")
+        return len(value)
+
+    def delete(self, key):
+        return self.data.pop(key, "")
+
+    def append(self, key, value):
+        if key in self.data:
+            self.data[key] += value
+        else:
+            self.data[key] = value
+        return "OK"
+
+# Example usage
+app = Application()
+print(app.ping())
+print(app.set("kunci", "satu"))
+print(app.append("kunci", "dua"))
+print(app.get("kunci"))
+print(app.strln("kunci"))
+print(app.delete("kunci"))
+print(app.get("kunci"))
